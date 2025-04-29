68511da29a0e81d212caa14c8c82808082a8afad
shaka_the_funk <shakaros0730@outlook.jp>
2025-04-29 22:19:40 +0900

    タブバー調整

--- app/components/FixedTabBar.tsx
+++ app/components/FixedTabBar.tsx
@@ -12,14 +12,14 @@ export default function FixedTabBar() {
     const profileIcon = pathname === "/profile" ? "/icons/home.png" : "/icons/home blank.png";
 
     return (
-        <div className="fixed bottom-0 left-0 right-0 bg-white flex justify-around items-center px-5 py-3">
-            <button onClick={() => router.push("/main")}>
+        <div className="fixed bottom-4 left-0 right-0 bg-white flex justify-around items-center px-5 py-3">
+            <button onClick={() => router.push("/main")} className="focus:outline-none">
                 <Image src={mainIcon} alt="Main" width={26} height={26} />
             </button>
-            <button onClick={() => router.push("/chat-list")}>
+            <button onClick={() => router.push("/chat-list")} className="focus:outline-none">
                 <Image src={chatIcon} alt="Chat" width={26} height={26} />
             </button>
-            <button onClick={() => router.push("/profile")}>
+            <button onClick={() => router.push("/profile")} className="focus:outline-none">
                 <Image src={profileIcon} alt="Profile" width={26} height={26} />
             </button>
         </div>
